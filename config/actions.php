a099ebb179e4364e05a35050821c892cb4f10139
mycodesgit <lowexian@gmail.com>
2023-07-10 11:06:11 +0800

    Update

--- config/actions.php
+++ config/actions.php
@@ -5,10 +5,10 @@ if( ! defined( 'ACCESS' ) ) die( 'DIRECT ACCESS NOT ALLOWED' );
 if (isset($_REQUEST['action'])) {
     $action = $_REQUEST['action'];
     
-    if (file_exists("./actions/{$action}.php")) {
-        include "./actions/{$action}.php";
+    if (file_exists("./app/actions/{$action}.php")) {
+        include "./app/actions/{$action}.php";
     } elseif (file_exists("../actions/{$action}.php")) {
-        include "../actions/{$action}.php";
+        include "../app/actions/{$action}.php";
     } else {
         // Handle the case when the action file doesn't exist
         // ...
